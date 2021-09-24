91485ad85b17408bb5b29328b07238b1f43a4081
Aswin Murali <aswinmurali.co@gmail.com>
2021-09-24 18:43:27 +0530

    adds pandas

---
+++ mlify/__main__.py
@@ -0,0 +1,5 @@
+from mlify.ui.gui.main import Mlify
+
+
+if __name__ == "__main__":
+    Mlify().mainloop()
---
+++ mlify/ui/gui/main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+class Mlify(tk.Tk):
+    def __init__(self,) -> None:
+        super().__init__()
+        self.mainloop()
