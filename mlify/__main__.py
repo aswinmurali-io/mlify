5619b2908192a9b205f5fb6ca0f48ec957f7df00
Aswin Murali <aswinmurali.co@gmail.com>
2021-10-28 19:51:49 +0530

    removes old code

--- mlify/__main__.py
+++
@@ -1,5 +0,0 @@
-from mlify.ui.gui.main import Mlify
-
-
-if __name__ == "__main__":
-    Mlify().mainloop()
--- mlify/ui/gui/main.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-class Mlify(tk.Tk):
-    def __init__(self,) -> None:
-        super().__init__()
-        self.mainloop()
